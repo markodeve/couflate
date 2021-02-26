3bf74d57a8829cafa1986e1ad4a0fd1d55827c89
leomark92 <markobogush@gmail.com>
2021-02-26 15:35:50 +0200

    odorlosec&menuadap

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 const deli = document.querySelector('.deli'),
-      overlay = document.querySelector('.menu__overlay');
+      overlay = document.querySelector('.menu__overlay'),
+      menu = document.querySelector('.menu__wrapper'),
+      close = document.querySelector('.close');
 
 deli.addEventListener('click', () =>  {
-    overlay.classList.add('active')
+    overlay.classList.add('active');
+    menu.classList.add('wrapper_active');
+});
+
+close.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    menu.classList.remove('wrapper_active');
 });
